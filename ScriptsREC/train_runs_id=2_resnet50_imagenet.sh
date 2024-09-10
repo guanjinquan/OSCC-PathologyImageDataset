1372a833c6796a0e587ebd2dd1cecb66201a2646
guanjinquan <1666320330@qq.com>
2024-09-10 13:06:11 +0800

    add-6-task-scripts

--- ScriptsREC/train_runs_id=2_resnet50_imagenet.sh
+++ ScriptsREC/train_runs_id=2_resnet50_imagenet.sh
@@ -1,6 +1,6 @@
 python /mnt/home/Guanjq/BackupWork/OSCC-PathologyImageDataset/Code/main_train.py \
-    --runs_id "004_TI_vit_small_p16_imagenet" \
-    --gpu_id "3" \
+    --runs_id "003_REC_resnet50_imagenet" \
+    --gpu_id "2" \
     --seed 109 \
     --weight_decay 6e-5 \
     --learning_rate 1e-6 \
@@ -11,7 +11,5 @@ python /mnt/home/Guanjq/BackupWork/OSCC-PathologyImageDataset/Code/main_train.py
     --datainfo_file "all_metadata.json" \
     --img_size 512 \
     --num_epochs 200 \
-    --model "vit_small_p16_imagenet" \
-    --use_tasks "['TI']" \
-    --augment_method 'vahadane' \
-    --stain_prob 0.5
+    --model "resnet50_imagenet" \
+    --use_tasks "['REC']"
