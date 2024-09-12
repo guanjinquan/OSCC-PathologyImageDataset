47600e8cb062806ec657daeea19dffe66dfc0045
guanjinquan <1666320330@qq.com>
2024-09-12 07:23:42 +0000

    server8-commit

--- ScriptsLNM/train_runs_id=2_resnet50_imagenet.sh
+++ ScriptsLNM/train_runs_id=2_resnet50_imagenet.sh
@@ -1,6 +1,6 @@
-python /mnt/home/Guanjq/BackupWork/OSCC-PathologyImageDataset/Code/main_train.py \
+python /home/Guanjq/Work/OSCC-PathologyImageDataset/Code/main_train.py \
     --runs_id "002_LNM_resnet50_imagenet" \
-    --gpu_id "2" \
+    --gpu_id "0" \
     --seed 109 \
     --weight_decay 6e-5 \
     --learning_rate 1e-6 \
