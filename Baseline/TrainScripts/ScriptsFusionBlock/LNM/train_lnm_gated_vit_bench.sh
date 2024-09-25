0b7b82fac22584afebe267e9f939fe235ba6b2f3
guanjinquan <1666320330@qq.com>
2024-09-25 11:33:34 +0800

    server4_to_s8

--- Baseline/TrainScripts/ScriptsFusionBlock/LNM/train_lnm_gated_vit_bench.sh
+++ Baseline/TrainScripts/ScriptsFusionBlock/LNM/train_lnm_gated_vit_bench.sh
@@ -1,6 +1,6 @@
 python /mnt/home/Guanjq/BackupWork/OSCC-PathologyImageDataset/Baseline/main_train.py \
     --runs_id "fusion_gated_002_LNM_vit_bench_32cluster" \
-    --gpu_id "0" \
+    --gpu_id "3" \
     --seed 109 \
     --weight_decay 6e-5 \
     --learning_rate 1e-6 \
