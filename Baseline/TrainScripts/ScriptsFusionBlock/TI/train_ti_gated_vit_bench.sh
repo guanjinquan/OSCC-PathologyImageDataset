57b3739993e39695c2a3a8c5359907bf9468869c
guanjinquan <1666320330@qq.com>
2024-09-28 02:07:15 +0800

    server4_update

--- Baseline/TrainScripts/ScriptsFusionBlock/TI/train_ti_gated_vit_bench.sh
+++ Baseline/TrainScripts/ScriptsFusionBlock/TI/train_ti_gated_vit_bench.sh
@@ -1,6 +1,6 @@
 python /mnt/home/Guanjq/BackupWork/OSCC-PathologyImageDataset/Baseline/main_train.py \
     --runs_id "fusion_gated_002_REC_vit_bench_32cluster" \
-    --gpu_id "0" \
+    --gpu_id "2" \
     --seed 109 \
     --weight_decay 6e-5 \
     --learning_rate 1e-6 \
@@ -10,7 +10,7 @@ python /mnt/home/Guanjq/BackupWork/OSCC-PathologyImageDataset/Baseline/main_trai
     --split_filename "split_seed=2024.json" \
     --datainfo_file "all_metadata.json" \
     --img_size 512 \
-    --num_epochs 200 \
+    --num_epochs 400 \
     --model "vit_small_p16_pathology" \
-    --use_tasks "['REC']" \
+    --use_tasks "['TE']" \
     --fusion_block 'gated'
