febd69f5b7eb6f97cd0a651bbd5cf06bc045df2a
guanjinquan <1666320330@qq.com>
2024-10-01 08:51:28 +0000

    server8-update

--- Baseline/TestScripts/ScriptsTI/train_runs_id=1_vit_bench.sh
+++ Baseline/TestScripts/ScriptsTI/train_runs_id=1_vit_bench.sh
@@ -1,4 +1,4 @@
-python /mnt/home/Guanjq/BackupWork/OSCC-PathologyImageDataset/Baseline/main_test.py \
+python /home/Guanjq/Work/OSCC-PathologyImageDataset/Baseline/main_test.py \
     --runs_id "001_TI_vit_bench_32cluster" \
     --gpu_id "1" \
     --seed 109 \
