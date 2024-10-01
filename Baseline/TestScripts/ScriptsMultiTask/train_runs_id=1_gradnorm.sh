1094ab78a206a4d44146bbff46bfdec5dd24b14a
guanjinquan <1666320330@qq.com>
2024-10-01 08:51:28 +0000

    server8-update

--- Baseline/TestScripts/ScriptsMultiTask/train_runs_id=1_gradnorm.sh
+++ Baseline/TestScripts/ScriptsMultiTask/train_runs_id=1_gradnorm.sh
@@ -1,4 +1,4 @@
-python /mnt/home/Guanjq/BackupWork/OSCC-PathologyImageDataset/Baseline/main_test.py \
+python /home/Guanjq/Work/OSCC-PathologyImageDataset/Baseline/main_test.py \
     --runs_id "001_GradNorm_vit_bench_32cluster" \
     --gpu_id "1" \
     --seed 109 \
