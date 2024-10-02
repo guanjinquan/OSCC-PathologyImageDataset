b5ec85868a03b057bacd848d521a9c30a7d62aae
guanjinquan <1666320330@qq.com>
2024-10-02 04:03:36 +0000

    server6 update

--- Baseline/TrainScripts/ScriptsTD/train_runs_id=6_denset121.sh
+++ Baseline/TrainScripts/ScriptsTD/train_runs_id=6_denset121.sh
@@ -1,12 +1,12 @@
 python /home/Guanjq/Work/OSCC-PathologyImageDataset/Baseline/main_train.py \
     --runs_id "006_TD_densenet121_imagenet" \
-    --gpu_id "1" \
+    --gpu_id "0" \
     --seed 109 \
     --weight_decay 6e-5 \
     --learning_rate 1e-6 \
     --backbone_lr 5e-7 \
-    --acc_step 16 \
-    --batch_size 1 \
+    --acc_step 8 \
+    --batch_size 2 \
     --split_filename "split_seed=2024.json" \
     --datainfo_file "all_metadata.json" \
     --img_size 512 \
