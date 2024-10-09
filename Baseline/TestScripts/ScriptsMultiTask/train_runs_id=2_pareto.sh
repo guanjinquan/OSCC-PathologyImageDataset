0738ab08ee35224cf643f2ddfa5291cbf327b710
guanjinquan <1666320330@qq.com>
2024-10-09 02:24:04 +0000

    server8 merge

--- Baseline/TestScripts/ScriptsMultiTask/train_runs_id=2_pareto.sh
+++ Baseline/TestScripts/ScriptsMultiTask/train_runs_id=2_pareto.sh
@@ -1,6 +1,6 @@
 python /home/Guanjq/Work/OSCC-PathologyImageDataset/Baseline/main_test.py \
     --runs_id "002_Pareto_vit_bench_32cluster" \
-    --gpu_id "1" \
+    --gpu_id "0" \
     --seed 109 \
     --weight_decay 6e-5 \
     --learning_rate 1e-6 \
@@ -10,6 +10,6 @@ python /home/Guanjq/Work/OSCC-PathologyImageDataset/Baseline/main_test.py \
     --split_filename "split_seed=2024.json" \
     --datainfo_file "all_metadata.json" \
     --img_size 512 \
-    --num_epochs 200 \
+    --num_epochs 400 \
     --model "vit_small_p16_pathology" \
     --use_tasks "['REC', 'LNM', 'TD', 'TI', 'CE', 'PI']"
---
+++ build_env.sh
@@ -0,0 +1,3 @@
+conda create -n Multi-OSCCPI python=3.10
+conda activate Multi-OSCCPI
+pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
